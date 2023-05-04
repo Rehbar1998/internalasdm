8d912b1fc45d42aa1eaa4a07b7e27af5052d6069
Rehbar1998 <rehbarkhan1998@gmail.com>
2023-05-04 23:31:19 +0530

    addfile

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Internal ASDM");
+		System.out.print("Hello Rehbar");
+		System.out.print("Hello Hiii");
+System.out.print("Hello ji");
+
+		
+	}
+
+}
